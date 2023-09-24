dae9802c785ce477a6b12b40caed8fe55673d470
melkior-nguyen <minhhieunguyen7397@gmail.com>
2023-09-24 21:10:13 +0700

    update

--- src/Pages/Layout/Layout.tsx
+++ src/Pages/Layout/Layout.tsx
@@ -5,8 +5,14 @@ import AppTable from '../../Components/AppTable/AppTable'
 function Layout() {
     return (
         <AppBox>
+            {/* Table */}
             <h1 style={{ color: 'green', fontSize: '16px' }}>App Table</h1>
             <AppTable />
+            {/* App content */}
+            <h1 style={{ color: 'green', fontSize: '16px' }}>App main content</h1>
+            {/* App Submit Form */}
+            <h1 style={{ color: 'green', fontSize: '16px' }}>App Submit Form</h1>
+
         </AppBox>
     )
 }
